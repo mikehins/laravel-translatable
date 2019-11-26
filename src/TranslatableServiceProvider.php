c8dda44f8ba747ffae613a2ae90710a89a727c02
Mike Hins <me@mikehins.com>
2019-11-26 20:21:44 +0000

    Apply fixes from StyleCI

--- src/TranslatableServiceProvider.php
+++ src/TranslatableServiceProvider.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: mikeh
  * Date: 2018-05-28
- * Time: 8:27 AM
+ * Time: 8:27 AM.
  */
 
 namespace Mikehins\Translatable;
@@ -13,21 +13,20 @@ use Illuminate\Support\ServiceProvider;
 
 class TranslatableServiceProvider extends ServiceProvider
 {
-	public function boot()
-	{
-		$this->loadMigrationsFrom(__DIR__ . '/database/migrations/');
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations/');
 //
-//		$this->publishes([
-//			__DIR__ . '/database/migrations/' => database_path('migrations')
-//		], 'migrations');
-		
-		Collection::macro('for', function ($field, $code) {
-			return $this->where('key', $field)->where('locale', $code)->pluck('value')->first() ?? $field;
-		});
-	}
-	
-	public function register()
-	{
-	
-	}
+        //		$this->publishes([
+        //			__DIR__ . '/database/migrations/' => database_path('migrations')
+        //		], 'migrations');
+
+        Collection::macro('for', function ($field, $code) {
+            return $this->where('key', $field)->where('locale', $code)->pluck('value')->first() ?? $field;
+        });
+    }
+
+    public function register()
+    {
+    }
 }
--- src/Translation.php
+++ src/Translation.php
@@ -6,80 +6,80 @@ use Illuminate\Database\Eloquent\Model;
 
 class Translation extends Model
 {
-	protected $table = 'translations';
-	
-	protected $primaryKey = 'translatable_id';
-	
-	protected $guarded = [];
-	
-	public static function getTableName()
-	{
-		return (new static)->getTable();
-	}
-	
-	public function store(array $attributes, array $values = [])
-	{
-		$this->deleteTranslation($attributes['translatable_type'], $attributes['translatable_id']);
+    protected $table = 'translations';
 
-		collect($attributes['translatable'])->each(function ($data, $locale) use ($attributes) {
-			collect($data)->each(function ($value, $key) use ($attributes, $locale) {
-				self::insert([
-					'key'               => $key,
-					'value'             => $value ?? '',
-					'locale'            => $locale,
-					'translatable_type' => $attributes['translatable_type'],
-					'translatable_id'   => $attributes['translatable_id'],
-				]);
-			});
-		});
-		
-		return $attributes['translatable_id'];
-	}
-	
-	public function deleteTranslation($type, $id)
-	{
-		$instance = self::where([
-			'translatable_type' => $type,
-			'translatable_id'   => $id
-		]);
-		
-		if ($instance) {
-			$instance->delete();
-		}
-	}
-	
-	/**
-	 * @param QueryBuilder $query
-	 * @param string $key
-	 * @param string $order
-	 * @return $query
-	 */
-	public function scopeOrderTranslationByKey($query, $key = 'name', $order = 'asc')
-	{
-		return $query->select(\DB::raw('
-			IF(translations.`key` = "' . $key . '", translations.value, "") as ' . $key . '
+    protected $primaryKey = 'translatable_id';
+
+    protected $guarded = [];
+
+    public static function getTableName()
+    {
+        return (new static)->getTable();
+    }
+
+    public function store(array $attributes, array $values = [])
+    {
+        $this->deleteTranslation($attributes['translatable_type'], $attributes['translatable_id']);
+
+        collect($attributes['translatable'])->each(function ($data, $locale) use ($attributes) {
+            collect($data)->each(function ($value, $key) use ($attributes, $locale) {
+                self::insert([
+                    'key'               => $key,
+                    'value'             => $value ?? '',
+                    'locale'            => $locale,
+                    'translatable_type' => $attributes['translatable_type'],
+                    'translatable_id'   => $attributes['translatable_id'],
+                ]);
+            });
+        });
+
+        return $attributes['translatable_id'];
+    }
+
+    public function deleteTranslation($type, $id)
+    {
+        $instance = self::where([
+            'translatable_type' => $type,
+            'translatable_id'   => $id,
+        ]);
+
+        if ($instance) {
+            $instance->delete();
+        }
+    }
+
+    /**
+     * @param QueryBuilder $query
+     * @param string $key
+     * @param string $order
+     * @return $query
+     */
+    public function scopeOrderTranslationByKey($query, $key = 'name', $order = 'asc')
+    {
+        return $query->select(\DB::raw('
+			IF(translations.`key` = "'.$key.'", translations.value, "") as '.$key.'
 		'))->where(
-			'translations.locale', '=', app()->getLocale()
-		)->orderBy(
-			\DB::raw($key . ' ' . $order)
-		)->groupBy('translations.translatable_id');
-	}
-	
-	public function translatable()
-	{
-		return $this->morphTo();
-	}
-	
-//	public function suggest($model, $key, $term)
-//	{
-//		return self::where('translations.locale', '=', app()->getLocale())
-//			->where('translations.key', '=', $key)
-//			->where('translations.value', 'LIKE', '%' . $term . '%')
-//			->where('translations.translatable_type', '=', $model);
-//	}
-	
-	public function getTranslatableAttribute()
-	{
-		return $this->attributes['translatable'] = request('translatable');
-	}
+            'translations.locale', '=', app()->getLocale()
+        )->orderBy(
+            \DB::raw($key.' '.$order)
+        )->groupBy('translations.translatable_id');
+    }
+
+    public function translatable()
+    {
+        return $this->morphTo();
+    }
+
+    //	public function suggest($model, $key, $term)
+    //	{
+    //		return self::where('translations.locale', '=', app()->getLocale())
+    //			->where('translations.key', '=', $key)
+    //			->where('translations.value', 'LIKE', '%' . $term . '%')
+    //			->where('translations.translatable_type', '=', $model);
+    //	}
+
+    public function getTranslatableAttribute()
+    {
+        return $this->attributes['translatable'] = request('translatable');
+    }
 }
